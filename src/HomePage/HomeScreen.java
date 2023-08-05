beee91fb7c9caad1823b17027adf2acf9db020ea
Ahmet Furkan KIZIL <furkan.kizil@ug.bilkent.edu.tr>
2023-08-05 17:38:17 +0300

    Form of the Home Screen is added

---
+++ src/HomePage/HomeScreen.java
@@ -0,0 +1,12 @@
+package HomePage;
+
+import javax.swing.*;
+
+public class HomeScreen {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+}
