9d12f86c8c3d6d8ec2dfd3f97e6acc04c0b34f47
Ahmet Furkan KIZIL <furkan.kizil@ug.bilkent.edu.tr>
2023-08-05 17:43:59 +0300

    Home Screen extended

--- src/HomePage/HomeScreen.java
+++ src/HomePage/HomeScreen.java
@@ -4,9 +4,15 @@ import javax.swing.*;
 
 public class HomeScreen {
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
+    private JButton lessonsButton;
+    private JButton studiesButton;
+    private JButton activitiesButton;
+    private JButton profileBoxButton;
+    private JButton filterBoxButton;
+    private JLabel homeLabel;
+    private JLabel messagesLabel;
+    private JLabel notificationsLabel;
+    private JLabel profileLabel;
+    private JLabel requestsLabel;
+    private JLabel profilePhotoLabel;
 }
